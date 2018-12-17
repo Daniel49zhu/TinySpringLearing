952cfd9ae50c7df84a775d5ba419581e0e98af02
Daniel49zhu <zhujiacheng94@163.com>
2018-12-17 23:26:36 +0800

    step5

---
+++ tiny_spring_step5_inject_bean_to_bean/src/main/java/us/codecraft/tinyioc/io/Resource.java
@@ -0,0 +1,12 @@
+package us.codecraft.tinyioc.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Resource是spring内部定位资源的接口。
+ *
+ */
+public interface Resource {
+    InputStream getInputStream() throws IOException;
+}
