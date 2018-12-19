2f7a7daa9f5bf96250fba804f1c682b03236b6d6
Daniel49zhu <zhujiacheng94@163.com>
2018-12-19 23:31:29 +0800

    step5

--- tiny_spring_step5_inject_bean_to_bean/src/test/java/us/codecraft/tinyioc/BeanFactoryTest.java
+++ tiny_spring_step5_inject_bean_to_bean/src/test/java/us/codecraft/tinyioc/BeanFactoryTest.java
@@ -11,6 +11,27 @@ import java.util.Map;
 public class BeanFactoryTest {
     @Test
     public void testLazy() throws Exception {
+        // 1.从资源接口根据文件名加载配置文件
+        //读取配置，将域值和引用分别存入beanfition中，再由reader中的Map对象来持有这些beanDefinition
+        //此时beandefinition中还没有相应的bean实例，引用字段也只有要引用的类的名字
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(new ResourceLoader());
+        xmlBeanDefinitionReader.loadBeanDefinitions("tinyioc.xml");
+
+        // 2.初始化BeanFactory并注册bean
+        //此时只是把reader中的属性值复制到了工厂的beandefinition的map中
+        AbstractBeanFactory beanFactory = new AutowireCapableBeanFactory();
+        for (Map.Entry<String, BeanDefinition> beanDefinitionEntry : xmlBeanDefinitionReader.getRegistry().entrySet()) {
+            beanFactory.registerBeanDefinition(beanDefinitionEntry.getKey(), beanDefinitionEntry.getValue());
+        }
+
+        // 3.获取bean
+        //getBean会实例化需要加载的bean，级联的创建对应的引用的实例
+        HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
+        helloWorldService.helloWorld();
+    }
+
+    @Test
+    public void testPreInstantiate() throws Exception {
         // 1.读取配置
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(new ResourceLoader());
         xmlBeanDefinitionReader.loadBeanDefinitions("tinyioc.xml");
@@ -21,7 +42,10 @@ public class BeanFactoryTest {
             beanFactory.registerBeanDefinition(beanDefinitionEntry.getKey(), beanDefinitionEntry.getValue());
         }
 
-        // 3.获取bean
+        // 3.初始化bean
+        beanFactory.preInstantiateSingletons();
+
+        // 4.获取bean
         HelloWorldService helloWorldService = (HelloWorldService) beanFactory.getBean("helloWorldService");
         helloWorldService.helloWorld();
     }
