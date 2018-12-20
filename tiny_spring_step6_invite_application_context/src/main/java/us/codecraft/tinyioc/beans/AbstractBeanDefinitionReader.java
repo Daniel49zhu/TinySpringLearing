2d6e2fadadf3f4795a17c2591f524c2128a5dcbe
Daniel49zhu <zhujiacheng94@163.com>
2018-12-21 00:05:33 +0800

    step6

--- tiny_spring_step6_invite_application_context/src/main/java/us/codecraft/tinyioc/beans/AbstractBeanDefinitionReader.java
+++ tiny_spring_step6_invite_application_context/src/main/java/us/codecraft/tinyioc/beans/AbstractBeanDefinitionReader.java
@@ -1,7 +1,27 @@
 package us.codecraft.tinyioc.beans;
 
+import us.codecraft.tinyioc.beans.io.ResourceLoader;
+
+import java.util.HashMap;
 import java.util.Map;
 
-public abstract class AbstractBeanDefinitionReader implements BeanDefinitionReader{
-    private Map<String,BeanDefinition> registry;
+public abstract class AbstractBeanDefinitionReader implements BeanDefinitionReader {
+
+    private Map<String, BeanDefinition> registry;
+
+    private ResourceLoader resourceLoader;
+
+    protected AbstractBeanDefinitionReader(ResourceLoader resourceLoader) {
+        this.registry = new HashMap<String, BeanDefinition>();
+        this.resourceLoader = resourceLoader;
+    }
+
+
+    public Map<String, BeanDefinition> getRegistry() {
+        return registry;
+    }
+
+    public ResourceLoader getResourceLoader() {
+        return resourceLoader;
+    }
 }
