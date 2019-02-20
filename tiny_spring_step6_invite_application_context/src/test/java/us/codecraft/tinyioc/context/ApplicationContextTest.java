ac202c33e8b71e2e79e529a1aa34e1b370d9c1c2
Daniel49zhu <zhujiacheng94@163.com>
2019-02-20 21:29:09 +0800

    step6

--- tiny_spring_step6_invite_application_context/src/test/java/us/codecraft/tinyioc/context/ApplicationContextTest.java
+++ tiny_spring_step6_invite_application_context/src/test/java/us/codecraft/tinyioc/context/ApplicationContextTest.java
@@ -10,6 +10,8 @@ public class ApplicationContextTest {
 
     @Test
     public void test() throws Exception {
+        //ApplicationContext会持有一个AutowireCapableBeanFactory对象，并在构造方法中调用refresh方法，来创建XmlBeanDefinitionReader来读取xml文件配置，
+        //并将组装的所有benaDefiniiton交给工厂对象
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tinyioc.xml");
         HelloWorldService helloWorldService = (HelloWorldService) applicationContext.getBean("helloWorldService");
         helloWorldService.helloWorld();
