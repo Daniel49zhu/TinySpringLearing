eadfc70acdef679ecdafba1bc13d1bbb9c59cba1
Daniel49zhu <zhujiacheng94@163.com>
2019-02-20 21:28:52 +0800

    step7

---
+++ tiny_spring_step7_method_interceptor_by_jdk_dynamic_proxy/src/main/java/us/codecraft/tinyioc/aop/TargetSource.java
@@ -0,0 +1,19 @@
+package us.codecraft.tinyioc.aop;
+
+public class TargetSource {
+    private Class targetClass;
+    private Object target;
+
+    public TargetSource(Object target,Class targetClass) {
+        this.targetClass = targetClass;
+        this.target = target;
+    }
+
+    public Class getTargetClass() {
+        return targetClass;
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+}
---
+++ tiny_spring_step7_method_interceptor_by_jdk_dynamic_proxy/src/test/java/us/codecraft/tinyioc/aop/JdkDynamicAopProxyTest.java
@@ -0,0 +1,29 @@
+package us.codecraft.tinyioc.aop;
+
+import org.junit.Test;
+import us.codecraft.tinyioc.HelloWorldService;
+import us.codecraft.tinyioc.context.ApplicationContext;
+import us.codecraft.tinyioc.context.ClassPathXmlApplicationContext;
+
+import java.sql.Time;
+
+public class JdkDynamicAopProxyTest {
+
+    @Test
+    public void testInterceptor() throws Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("tinyioc.xml");
+        HelloWorldService helloWorldService = (HelloWorldService) applicationContext.getBean("helloWorldService");
+
+        AdvisedSupport advisedSupport = new AdvisedSupport();
+        TargetSource targetSource = new TargetSource(helloWorldService,HelloWorldService.class);
+        advisedSupport.setTargetSource(targetSource);
+
+        TimerInterceptor timerInterceptor = new TimerInterceptor();
+        advisedSupport.setMethodInterceptor(timerInterceptor);
+
+        JdkDynamicAopProxy jdkDynamicAopProxy = new JdkDynamicAopProxy(advisedSupport);
+        HelloWorldService helloWorldService1Proxy = (HelloWorldService) jdkDynamicAopProxy.getProxy();
+
+        helloWorldService1Proxy.helloWorld();
+    }
+}
